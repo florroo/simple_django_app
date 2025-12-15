41bc3ade14613fcd66f03b71b590e800cb2921db
Bartłomiej Florek <florekb95@gmail.com>
2025-12-15 14:17:57 +0100

    admin panel

--- simple_app/documents/models.py
+++ simple_app/documents/models.py
@@ -6,3 +6,8 @@ class Document(models.Model):
     title = models.CharField(max_length = 75)
     category = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+        
+    
--- simple_app/documents/views.py
+++ simple_app/documents/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Document
 
 # Create your views here.
 def documents_list(request):
-    return render(request, 'documents/documents_list.html')
+    documents = Document.objects.all().order_by('-date') # - in descending order
+    return render(request, 'documents/documents_list.html', { 'documents': documents })
