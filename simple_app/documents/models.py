2d184a58b039dc1e58d097fc5c4301628261f9c5
Bartłomiej Florek <florekb95@gmail.com>
2025-12-21 23:53:42 +0100

    uploading files and new documents, everyone has own documents and cant look at others

--- simple_app/documents/models.py
+++ simple_app/documents/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Document(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "documents")
     title = models.CharField(max_length = 75)
-    category = models.CharField(max_length = 50)
     description = models.TextField(blank = True)
     date = models.DateTimeField(auto_now_add = True)
     slug = models.SlugField(blank = True)
-    banner = models.ImageField(default='default.png', blank = True)
+    file = models.FileField(blank = True, null = True)
+
+    class Meta:
+        unique_together = ("owner", "slug")
 
     def __str__(self):
         return self.title
--- simple_app/documents/views.py
+++ simple_app/documents/views.py
@@ -1,19 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Document
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.utils.text import slugify
 
 # Create your views here.
 @login_required(login_url='/users/login/')
 def documents_list(request):
-    documents = Document.objects.all().order_by('-date') # - in descending order
+    documents = Document.objects.filter(owner=request.user).order_by('-date') # - in descending order
     return render(request, 'documents/documents_list.html', { 'documents': documents })
+
 
 @login_required(login_url='/users/login/')
 def document_page(request, slug):
-    document = Document.objects.get(slug=slug)
+    document = get_object_or_404(Document, slug=slug, owner=request.user)
     return render(request, 'documents/document_page.html', { 'document': document })
+
 
 @login_required(login_url='/users/login/')
 def document_new(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        file = request.FILES.get('file')
+
+        if not title or not file:
+            return HttpResponse("title and file are required", status=400)
+
+        document = Document.objects.create(
+            owner = request.user,
+            title = title,
+            description = description,
+            file = file,
+            slug = slugify(title)
+        )
+
+        return redirect('documents:list')
+
     return render(request, 'documents/document_new.html')
