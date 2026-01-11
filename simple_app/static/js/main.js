311d13c4b583b564c242730013eb2b7ee9cf76b0
Bartłomiej Florek <florekb95@gmail.com>
2026-01-11 01:27:17 +0100

    dark and light theme switch

--- simple_app/static/js/main.js
+++ simple_app/static/js/main.js
@@ -1,1 +1,23 @@
-console.log("js from about page")
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("button_theme");
+    const body = document.body;
+
+    if (localStorage.getItem("theme") === "light") {
+        body.classList.add("light");
+        toggle.textContent = "🌙";
+    } else {
+        toggle.textContent = "☀️";
+    }
+
+    toggle.addEventListener("click", () => {
+        body.classList.toggle("light");
+
+        if (body.classList.contains("light")) {
+            localStorage.setItem("theme", "light");
+            toggle.textContent = "🌙";
+        } else {
+            localStorage.setItem("theme", "dark");
+            toggle.textContent = "☀️";
+        }
+    });
+});
